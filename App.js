d8e374411165f6f2dfbefb5a0a367435b5e145fe
Animish2002 <animishchopade123@gmail.com>
2024-04-07 23:26:26 +0530

    lecture 2 (Ingniting the app)

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", xyz: "kinee" },
 //   "hello world from h1 tag!!"
 // );
+
+
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -12,6 +18,8 @@ const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "Hey there from H1 tag!!!"),
     React.createElement("h2", {}, "Hey there from H2 tag!!!"),
+    React.createElement("h2", {}, "Hey there whats up baby girl!!!"),
+
   ]),
 ]);
 
---
+++ README.md
@@ -0,0 +1,19 @@
+# Namaste React🚀
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HNR = Hot Module Replacement
+- File watching algorithms - written in c++
+- Catching - Faster Builds
+- Image Optimization
+- Minification of the files
+- Bundling
+- Compress the files
+- Consiting Hashing    
+- Code Splitting 
+- Differential Bundling - to support all browser as well as older browsers
+- Error Handling
+- Https works
+- Tree Shaking - removes unsed code from the react app
